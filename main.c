10a492dec98cd24df47ccc6a19c218708a709bb8
null64x <72575621+null64x@users.noreply.github.com>
2021-08-20 02:56:58 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 #include "dummy.h"
 
 // Defines used to check if call is really coming from client
-#define baseOperation 0x7653
-#define VARIABLE_NAME L"TrDrVcKlZ"
+#define baseOperation 0x6256
+#define VARIABLE_NAME L"keRdjvbgC"
 
 //This is only to modify every command/magic key with only 1 def and don't need to go everywhere, the compiler will automatically parse the operation to number
-#define COMMAND_MAGIC baseOperation*0x6241
+#define COMMAND_MAGIC baseOperation*0x7346
 
 
 // Dummy protocol struct
@@ -48,7 +48,7 @@ typedef int (MicrosoftCallingType* MmCopyVirtualMemory)(
 
 // Our protocol GUID (should be different for every driver)
 static const EFI_GUID ProtocolGuid
-= { 0x4f12753e, 0xfd4e, 0x4021, {0x9e, 0x8e, 0x30, 0xa4, 0xa2, 0x9b, 0x64, 0xf2} };
+= { 0x2f84893e, 0xfd5e, 0x2038, {0x8d, 0x9e, 0x20, 0xa7, 0xaf, 0x9c, 0x32, 0xf1} };
 
 // VirtualAddressMap GUID (gEfiEventVirtualAddressChangeGuid)
 static const EFI_GUID VirtualGuid
